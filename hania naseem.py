d503a9f9cb18b05e21ead390208b00e185926a85
hanianaseem <hanianaseem15@gmail.com>
2024-10-30 20:02:27 +0500

    Add files via upload

---
+++ hania naseem.py
@@ -0,0 +1,49 @@
+#calculate the addition of three numbers:
+number1=7
+number2=15
+number3=49
+sum=(number1+number2+number3)    #71
+print(sum)       #71
+
+#calculate the substraction of three numbers:
+number1=8
+number2=29
+number3=19
+substraction=(number1-number2-number3) #-40
+print(substraction)     #-40
+
+#calculate the product:
+number1=17
+number2=18
+number3=16
+product=(number1*number2*number3) #4896
+print(product)   #4896
+
+#calculate the average:
+number1=47
+number2=50
+number3=78
+average=(number1+number2+number3)/3 #58.333
+print(average)    #58.333
+
+#calculate the area of triangle:
+base=44
+height=70
+area=(0.5*base*height) #1540
+print(area)    #1540
+
+#calculate the rectangle:
+lenght=48
+widght=66
+area=(lenght*widght) #3168
+print(area)   #3168
+
+#calculate the sqare:
+side=10
+area=(side*side) #100
+print(area)     #100
+
+#calculate the radius:
+radius=15
+area=(3.14*radius**2) #706.5
+print(area)     #706.5
